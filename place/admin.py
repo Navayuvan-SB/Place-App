f6297001fbad083ae4837c16971eb11816a77ef4
navayuvansb <sb.navayuvan@gmail.com>
2021-04-09 18:19:04 +0530

    Add Place model in admin panel

--- place/admin.py
+++ place/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Place, PlaceType
 
-# Register your models here.
+
+class PlaceTypeInlineAdmin(admin.TabularInline):
+    model = PlaceType
+    extra = 2
+
+
+@admin.register(Place)
+class PlaceAdmin(OSMGeoAdmin):
+    list_display = ('title', 'location')
+    inlines = [PlaceTypeInlineAdmin]
