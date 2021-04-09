f3c58272ec554bcc9e61c7d8c9d1c9d732ad7682
navayuvansb <sb.navayuvan@gmail.com>
2021-04-09 21:04:03 +0530

    Add place add, edit, delete routes

--- place/urls.py
+++ place/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.PlaceListView.as_view(), name='places'),
-    path('<int:pk>', views.PlaceDetailView.as_view(), name='place-detail')
+    path('<int:pk>', views.PlaceDetailView.as_view(), name='place-detail'),
+    path('create/', views.AddPlaceView.as_view(), name='add-place'),
+    path('<int:pk>/edit', views.EditPlaceView.as_view(), name='edit-place'),
+    path('<int:pk>/delete', views.DeletePlaceView.as_view(), name='delete-place')
 ]
