953b8dcbbb20850545c22180f4fa096cf13b9507
navayuvansb <sb.navayuvan@gmail.com>
2021-04-09 18:18:57 +0530

    Add Place & PlaceType model

--- place/models.py
+++ place/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+from django.contrib.gis.db import models
+from django.contrib.gis import forms
 
-# Create your models here.
+
+class Place(models.Model):
+
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
+    location = models.PointField()
+
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    phone = models.CharField(max_length=100)
+
+
+class PlaceType(models.Model):
+
+    place_type = models.CharField(
+        max_length=100, verbose_name='Type of the place')
+
+    place = models.ForeignKey('Place', on_delete=models.SET_NULL, null=True)
